301dc8f0321bec57c52f2e181621ca8857ab7364
Deol Satish <164171952+deol-satish@users.noreply.github.com>
2024-09-16 12:36:27 +1000

    add expool class in run_plm.py

--- adaptive_bitrate_streaming/run_plm.py
+++ adaptive_bitrate_streaming/run_plm.py
@@ -26,6 +26,27 @@ from plm_special.utils.plm_utils import load_plm
 from plm_special.utils.console_logger import ConsoleLogger
 
 
+class ExperiencePool:
+    """
+    Experience pool for collecting trajectories.
+    """
+    def __init__(self):
+        self.states = []
+        self.actions = []
+        self.rewards = []
+        self.dones = []
+
+    def add(self, state, action, reward, done):
+        self.states.append(state)  # sometime state is also called obs (observation)
+        self.actions.append(action)
+        self.rewards.append(reward)
+        self.dones.append(done)
+
+    def __len__(self):
+        return len(self.states)
+
+
+
 PLM_LAYER_SIZES = {
     'gpt2': {
         'base': 24,
